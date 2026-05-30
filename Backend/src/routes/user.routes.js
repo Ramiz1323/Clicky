2494baab63bb61138cc7a266263d57da90a2b025
Sk Ramiz Raza <reenabibi5678@gmail.com>
2026-05-30 23:27:32 +0530

    feat: add comments to user routes

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -2,7 +2,20 @@ const userRouter = require('express').Router();
 const { followUser, unfollowUser } = require('../controllers/user.controller.js');
 const { identifyUser } = require('../middlewares/auth.middleware.js');
 
+/**
+ * POST /api/user/follow/:username
+ * Description: Follow a user
+ * Protected: Yes
+ * 
+ */
 userRouter.post('/follow/:username', identifyUser, followUser);
+
+/**
+ * POST /api/user/unfollow/:username
+ * Description: Unfollow a user
+ * Protected: Yes
+ * 
+ */
 userRouter.post('/unfollow/:username', identifyUser, unfollowUser);
 
 module.exports = userRouter;
